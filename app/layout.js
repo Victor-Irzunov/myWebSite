44a7b17bef8f5039819a7d4a6152a2b908c54b03
Виктор <viktor@MacBook-Air-Viktor.local>
2023-11-26 10:43:36 +0300

    masha

--- app/layout.js
+++ app/layout.js
@@ -17,20 +17,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <head>
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" /> */}
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
-
         <meta name="theme-color" content="#317EFB" />
-        {/* <link rel="android-chrome-192x192" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
-        <link rel="android-chrome-512x512" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="shortcut icon" href="/favicon/apple-touch-icon.png" /> */}
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
-
-        {/* <link rel="canonical" href="https://vi-tech.by/" /> */}
         <meta name="google-site-verification" content="Eo0Xif5nmQIm7hOzoU1kzcVeqrcRlDBiDWa5au4Yxz4" />
         <meta name="yandex-verification" content="4c1774881f86806f" />
 
--- constans/portfolio/PortfolioData.js
+++ constans/portfolio/PortfolioData.js
@@ -405,4 +405,17 @@ export const dataPortfolio = [
 		w2: 70,
 		h2: 132
 	},
+	{
+		id: 33,
+		title: '',
+		link: 'https://irzunova.by/',
+		image: '/portfolio/portfolioCard/masha-production/1.webp',
+		alt: 'Сайт клиента психолог в Минске, компании VI:TECH',
+		w: 1000,
+		h: 566,
+		image2: '/portfolio/portfolioCard/masha-production/2.webp',
+		alt2: 'Сайт клиента психолог в Минске, компании VI:TECH',
+		w2: 70,
+		h2: 132
+	},
 ]
