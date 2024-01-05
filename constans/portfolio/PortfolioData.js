afd6dab15d6d31401d18e98d1dcf576b2f698e64
Виктор <viktor@MacBook-Air-Viktor.local>
2024-01-05 20:14:41 +0300

    plumber2

--- constans/portfolio/PortfolioData.js
+++ constans/portfolio/PortfolioData.js
@@ -496,4 +496,17 @@ export const dataPortfolio = [
 		w2: 70,
 		h2: 132
 	},
+	{
+		id: 40,
+		title: '',
+		link: 'https://plumber-minsk.vercel.app/',
+		image: '/portfolio/portfolioCard/plumber2/1.webp',
+		alt: 'Сайт клиента услуги сантехника, компании VI:TECH',
+		w: 1000,
+		h: 566,
+		image2: '/portfolio/portfolioCard/plumber2/2.webp',
+		alt2: 'Сайт клиента услуги сантехника, компании VI:TECH',
+		w2: 70,
+		h2: 132
+	},
 ]
