9b17a2bcaea15f5ef94233fc161e7466f1686188
Виктор <viktor@MacBook-Air-Viktor.local>
2024-03-26 12:30:52 +0300

    -2new

--- constans/portfolio/PortfolioData.js
+++ constans/portfolio/PortfolioData.js
@@ -702,4 +702,17 @@ export const dataPortfolio = [
 		w2: 70,
 		h2: 132
 	},
+	{
+		id: 53,
+		title: 'evakuator-snowy.vercel.app',
+		link: 'https://evakuator-snowy.vercel.app/',
+		image: '/portfolio/portfolioCard/evakuator2/1.webp',
+		alt: 'Сайт клиента услуги эвакуатора, компании VI:TECH',
+		w: 1000,
+		h: 566,
+		image2: '/portfolio/portfolioCard/evakuator2/2.webp',
+		alt: 'Сайт клиента услуги эвакуатора, компании VI:TECH',
+		w2: 70,
+		h2: 132
+	},
 ]
