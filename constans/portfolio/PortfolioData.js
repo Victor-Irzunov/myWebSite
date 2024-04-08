5d19af2110117a7c4e41fac9ddd78b0b16e798d7
Виктор <viktor@MacBook-Air-Viktor.local>
2024-04-08 10:26:44 +0300

    cofe-intenso

--- constans/portfolio/PortfolioData.js
+++ constans/portfolio/PortfolioData.js
@@ -715,4 +715,17 @@ export const dataPortfolio = [
 		w2: 70,
 		h2: 132
 	},
+	{
+		id: 54,
+		title: 'evakuator-snowy.vercel.app',
+		link: 'https://intenso.by/',
+		image: '/portfolio/portfolioCard/cofe-intenso/1.webp',
+		alt: 'Сайт клиента продажа кофе, компании VI:TECH',
+		w: 1000,
+		h: 566,
+		image2: '/portfolio/portfolioCard/cofe-intenso/2.webp',
+		alt: 'Сайт клиента продажа кофе, компании VI:TECH',
+		w2: 70,
+		h2: 132
+	},
 ]
