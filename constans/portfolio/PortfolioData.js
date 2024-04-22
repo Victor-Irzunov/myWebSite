29ff053f53edb3e7266541163ebc34b34b92dbd4
Виктор <viktor@MacBook-Air-Viktor.local>
2024-04-22 20:01:36 +0300

    sportzal

--- constans/portfolio/PortfolioData.js
+++ constans/portfolio/PortfolioData.js
@@ -767,4 +767,17 @@ export const dataPortfolio = [
 		w2: 70,
 		h2: 132
 	},
+	{
+		id: 58,
+		title: 'sportzal.vercel.app/',
+		link: 'https://sportzal.vercel.app/',
+		image: '/portfolio/portfolioCard/sportzal/1.webp',
+		alt: 'Сайт клиента Тренажерный зал в Минске, компании VI:TECH',
+		w: 1000,
+		h: 566,
+		image2: '/portfolio/portfolioCard/sportzal/2.webp',
+		alt: 'Сайт клиента Тренажерный зал в Минске, компании VI:TECH',
+		w2: 70,
+		h2: 132
+	},
 ]
